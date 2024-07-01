96f550fa14bf73ef2746cd208d5751b9810e7d24
watercat <watercat0330@gmail.com>
2024-07-01 16:01:28 +0800

    fix dayselect

--- src/components/widgets/dayselect.vue
+++ src/components/widgets/dayselect.vue
@@ -17,8 +17,6 @@ import { selectedDay } from '@/reactive/cart'
 
 const now = DateTime.now().setZone('Asia/Taipei')
 const sysNow = now.hour>=13 ? now.plus({days: 1}) : now
-selectedDay.day = sysNow
-
 
 const emit = defineEmits(['update'])
 
--- src/reactive/cart.js
+++ src/reactive/cart.js
@@ -1,6 +1,10 @@
 import { reactive } from 'vue'
+import { DateTime } from 'luxon'
+
+const now = DateTime.now().setZone('Asia/Taipei')
+const sysNow = now.hour>=13 ? now.plus({days: 1}) : now
 
 export const cart = reactive([])
 export const selectedDay = reactive({
-    day: ''
+    day: sysNow
 })
